b4f44d0b34eb2bad4db9e3d5650332ee5b3396fb
pankaj <punkkauthority@gmail.com>
2020-10-02 16:03:00 +0530

    first-commit

--- public/index.js
+++ public/index.js
@@ -6,6 +6,16 @@
 var connection = new RTCMultiConnection();
 
 connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+// use your own TURN-server here!
+connection.iceServers = [{
+    'urls': [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun.l.google.com:19302?transport=udp',
+    ]
+  }];
+ 
 $( document ).ready(function() {
 
 
